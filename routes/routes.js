cd2ac31a30a985623b7579c23a7e455c4779054d
lilianma213 <lilianma213@gmail.com>
2015-01-22 10:12:58 -0500

    following fb instructions

--- routes/routes.js
+++ routes/routes.js
@@ -71,4 +71,33 @@ router.post('/pastTrips', function(req, res) {
     }
 });
 
+/*error handling chunk of code*/
+
+router.get('/404', function(req, res, next){
+  // trigger a 404 since no other middleware
+  // will match /404 after this one, and we're not
+  // responding here
+  next();
+});
+
+router.get('/403', function(req, res, next){
+  // trigger a 403 error
+  var err = new Error('not allowed!');
+  err.status = 403;
+  next(err);
+});
+
+router.get('/500', function(req, res, next){
+  // trigger a generic (500) error
+  next(new Error('keyboard cat!'));
+});
+
+router.use(function(req, res, next){
+  res.status(404);
+  res.render('404', {});
+});
+
+/*error handling chunk of code end*/
+
+
 module.exports = router;
