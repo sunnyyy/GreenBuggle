8653c5344e5267c325dd708ef3d7c77f31892549
lilianma213 <lilianma213@gmail.com>
2015-01-22 18:52:31 -0500

    fb

--- app.js
+++ app.js
@@ -118,29 +118,23 @@ passport.deserializeUser(function(obj, done) {
 
     },
     function(req, token, refreshToken, profile, done) {
+
+        // asynchronous
+        process.nextTick(function() {
+
             // check if the user is already logged in
+            if (!req.user) {
+                console.log("checking login");    
                 User.findOne({ 'facebook_id' : profile.id }, function(err, user) {
                     if (err)
                         return done(err);
 
-                    if (user) {
-
-                        // if there is a user id already but no token (user was linked at one point and then removed)
-                        if (!user.facebook.token) {
-                            name  = profile.name.givenName;
-                            user.save(function(err) {
-                                if (err)
-                                    return done(err);
-                                    
-                                return done(null, user);
-                            });
-                        }
-
+                    if (user) {                        
                         return done(null, user); // user found, return that user
                     } else {
                         // if there is no user, create them
                         console.log("creating new user in database");
-                        var newUser            = new User();
+                        var newUser = new User();
                         facebook_id    = profile.id;
                         name  = profile.name.givenName;
                        
@@ -153,7 +147,23 @@ passport.deserializeUser(function(obj, done) {
                     }
                 });
 
-  
+            } else {
+                // user already exists and is logged in, we have to link accounts
+                var user            = req.user; // pull the user out of the session
+
+                user.facebook.id    = profile.id;
+                user.facebook.token = token;
+                user.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
+                user.facebook.email = (profile.emails[0].value || '').toLowerCase();
+
+                user.save(function(err) {
+                    if (err)
+                        return done(err);
+                        
+                    return done(null, user);
+                });
+
+            }
         });
 
     }));
