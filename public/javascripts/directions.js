8d535c09a58f171ce0a0afe5cf16d32d18312c17
pistachiosgreen <jsguo9@gmail.com>
2015-01-27 11:51:57 -0500

    idk
    idk

--- public/javascripts/directions.js
+++ public/javascripts/directions.js
@@ -342,8 +342,8 @@ function turnGreen() { //takes in seconds as parameters
     document.getElementById('driveButton').className = 'btn btn-primary';
     document.getElementById('transitButton').className = 'btn btn-primary';
     document.getElementById('walkButton').className = 'btn btn-success';
-  } else if ((t < 18000 && t < 1.5*c) || t < 3600) {
-    //transit is green if it takes less than half an hour 
+  } else if (((t < 18000) && (t < 1.5*c) )|| (t < 3600)) {
+    //transit is green if it takes less than an hour 
     document.getElementById('bcirc_car').style.background = '#06c';
     document.getElementById('bcirc_transit').style.background = '#093';
     document.getElementById('bcirc_walk').style.background = '#06c';
